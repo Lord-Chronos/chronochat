27caec4e20a74572b71f0947d32f968d294c25c8
Joshua Murray <j.murray567@gmail.com>
2022-12-13 20:22:35 +0000

    Introduced error handling

--- resources/views/layouts/basic.blade.php
+++ resources/views/layouts/basic.blade.php
@@ -3,6 +3,17 @@
         <title> @yield('title') </title>
     </head>
     <body>
+    @if($errors->any())
+        <div>
+            Errors:
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    <div>
         @yield('content')
-    </body>
+    </div>
 </html>
