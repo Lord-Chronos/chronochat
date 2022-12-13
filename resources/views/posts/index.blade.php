a18e2ba9d7d8c674a8e78f6ce46f7cfdb17f9f1b
Joshua Murray <j.murray567@gmail.com>
2022-12-13 20:22:22 +0000

    Add link to create on main page

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,26 +1,27 @@
 @extends('layouts.basic')
 @section('title', 'Posts')
 @section('content')
-<h1>Posts for the site:</h1>
-<br>
-<ul>
-    @foreach ($posts as $post)
-
-    <li> {{$post->title }} <b>Posted by {{$post->user->name}} on {{$post->created_at}}</b></li>
-         {{$post->content }}
+    <h1>Posts for the site:</h1>
     <br>
+    <ul>
+        @foreach ($posts as $post)
 
-    <ol>
-        @foreach ($post->comment as $comment)
-            <li> <b>{{$comment->user->name}} replied:</b> {{$comment->content }}</li>
-    
+        <li> {{$post->title }} <b>Posted by {{$post->user->name}} on {{$post->created_at}}</b></li>
+            {{$post->content }}
+        <br>
+
+        <ol>
+            @foreach ($post->comment as $comment)
+                <li> <b>{{$comment->user->name}} replied:</b> {{$comment->content }}</li>
+        
+            @endforeach
+        </ol>
+        <br>
+        <br>
+
         @endforeach
-    </ol>
-    <br>
-    <br>
 
-    @endforeach
-
-</ul>
+    </ul>
+    <a href="{{ route('posts.create')}}">Create Post</a>
 
 @endsection
