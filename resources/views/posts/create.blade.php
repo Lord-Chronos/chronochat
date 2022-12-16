1f19707c4a7980f706130cb6624b82a469f7426c
Joshua Murray <j.murray567@gmail.com>
2022-12-16 09:39:04 +0000

    Removed styling in line

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -5,12 +5,15 @@
 @section('content')
     <h1>Form for Posts:</h1>
 
-    <form method="post" action="{{ route('posts.store')}}">
-        @csrf
-        <p>Title <input type="text" name="title" value={{ old('title') }}></p>
-        <p>Content <input type="text" name="content" value={{ old('content') }}></p>
-        <input type="submit" value="Submit">
-        <a href="{{ route('posts.index') }}">Cancel</a>
-    </form>
+    <div style="display: flex;">
+        <form method="post" action="{{ route('posts.store') }}">
+            @csrf
+            <p>Title<br><input type="text" name="title" value={{ old('title') }}></p>
+            <p>Content<br><input type="text" name="content" value={{ old('content') }}></p>
+            <input type="hidden" name="user_id" value="{{ Auth::id() }}">
+            <input type="submit" value="Submit">
+            <a href="{{ route('posts.index') }}">Cancel</a>
+        </form>
+    </div>
 
 @endsection
