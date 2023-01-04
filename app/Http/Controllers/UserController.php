7237030287f4f8f8d6e22f2e029db6600a95d25f
Joshua Murray <j.murray567@gmail.com>
2023-01-04 21:37:43 +0000

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -40,7 +40,7 @@ class userController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request, $id)
+    public function store(Request $request)
     {
         //dd(request()->all());
         $validated = $request->validate([
