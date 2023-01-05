ae091c6ee1f273ced31b436fe830172c2026b1de
Joshua Murray <j.murray567@gmail.com>
2023-01-05 23:54:20 +0000

    Made forms better

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -4,18 +4,26 @@
 
 @section('content')
     <h1>Form for Posts:</h1>
-            <div class="post box">
+    <div class="post box">
 
-    <div style="display: flex;">
-        <form method="post" action="{{ route('posts.store') }}">
-            @csrf
-            <p>Title<br><input type="text" name="title" value={{ old('title') }}></p>
-            <p>Content<br><input type="text" name="content" value={{ old('content') }}></p>
-            <input type="hidden" name="user_id" value="{{ Auth::id() }}">
-            <input type="submit" value="Submit">
-            <a href="{{ route('posts.index') }}">Cancel</a>
-        </form>
+        <div class="grid grid-rows-1 grid-flow-col gap-0">
+
+            <form method="post" action="{{ route('posts.store') }}" enctype="multipart/form-data">
+                @csrf
+                <h1 class="text-xl">Title<br><input type="text" name="title" placeholder="Title" value={{ old('title') }}>
+                </h1>
+                <h1 class="text-xl">Content<br><input type="text" name="content" placeholder="Content"
+                        value={{ old('content') }}></h1>
+                <input type="hidden" name="user_id" value="{{ Auth::id() }}">
+                <input type="file" name="image" class="form-control">
+
+                <input type="submit" value="Submit" class='button'>
+
+
+            </form>
+
+        </div>
     </div>
-    </div>
+                <a href="{{ route('posts.index') }}" class='button'>Cancel</a>
 
 @endsection
