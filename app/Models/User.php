b754292645d7feb80cf158e753becd1e29c8d848
Joshua Murray <j.murray567@gmail.com>
2023-01-06 23:57:39 +0000

    Cleanup

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,6 +14,7 @@ class User extends Authenticatable
     public function post(){
         return $this->hasMany(Post::class);
     }
+    
     public function comment(){
         return $this->hasMany(Comment::class);
     }
